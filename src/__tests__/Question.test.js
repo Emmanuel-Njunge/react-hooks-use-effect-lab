fa0e7aa067a548a482b700d1975b9f10a536e83e
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-05-01 14:49:36 +0300

    Completed React useEffect Lab and fixed interval cleanup

--- src/__tests__/Question.test.js
+++ src/__tests__/Question.test.js
@@ -21,11 +21,10 @@ afterEach(() => {
   jest.useRealTimers();
 });
 
-// const onChange = jest.fn();
-test("creates an interval with setTimeout", () => {
-  jest.spyOn(global, 'setTimeout');
+test("creates an interval with setInterval", () => {
+  jest.spyOn(global, "setInterval"); // 🔄 Fix spy
   render(<Question question={testQuestion} onAnswered={noop} />);
-  expect(setTimeout).toHaveBeenCalled();
+  expect(setInterval).toHaveBeenCalled();
 });
 
 test("decrements the timer by 1 every second", () => {
@@ -54,11 +53,11 @@ test("calls onAnswered after 10 seconds", () => {
   expect(onAnswered).toHaveBeenCalledWith(false);
 });
 
-test("clears the timeout after unmount", () => {
-  jest.spyOn(global, 'clearTimeout');
+test("clears the interval after unmount", () => {
+  jest.spyOn(global, "clearInterval"); // 🔄 Fix spy
   const { unmount } = render(
     <Question question={testQuestion} onAnswered={noop} />
   );
   unmount();
-  expect(clearTimeout).toHaveBeenCalled();
+  expect(clearInterval).toHaveBeenCalled(); // 🔄 Fix assertion
 });
